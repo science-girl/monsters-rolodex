c64cfd807198688623ef48b47ac181988e248d03
LindseyW <Lindsey@LindseyWs-MacBook-Pro.local>
2020-11-01 18:41:52 -0800

    Updates

---
+++ precache-manifest.6f5d80ebee9d6358f7a2a5dc5ca5f529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42db2b0915fb66fc9cf4d685ba64b84a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3215a4a71bda4f104389",
+    "url": "/monsters-rolodex/static/css/main.2b89c6c8.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3215a4a71bda4f104389",
+    "url": "/monsters-rolodex/static/js/main.8dcd737e.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
